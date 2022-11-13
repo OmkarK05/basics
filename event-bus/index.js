6125c607b73cc9a17946b82043fed487f3ef63c3
omkar kesarkhane <omkesarkhane@gmail.com>
2022-11-13 23:12:19 +0530

    initial commit

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 let app = express();
 app.use(bodyParser.json());
 
-app.post("/events", async (req, res) => {
+app.post('/events', async (req, res) => {
   const event = req.body;
 
   // axios.post("http://localhost:4000/events", event);
   // axios.post("http://localhost:4001/events", event);
-  let response = await axios.post("http://localhost:4004/events", event);
+  let response = await axios.post('http://localhost:4004/events', event);
   console.log(response);
   if (response.status === 200) {
-    res.send({ status: "OK" });
+    res.send({ status: 'OKkk' });
   }
 });
 
-app.listen("4003", () => {
-  console.log("Running on port 4005");
+app.listen('4003', () => {
+  console.log('Running on port 4005');
 });
